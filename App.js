5262c71a69c13ac6950ab4bb9d65d6e530448e5c
Daniele <daniele.rb6@gmail.com>
2017-09-24 10:24:46 +0200

    feat: Add Redux to the application

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <AddEntry />
-      </View>
+      /*
+       * In order to hook up Redux to our applications, wrap everything
+       * insed a Provider.
+       */
+      <Provider store={createStore(reducer)}>
+        <View>
+          <AddEntry />
+        </View>
+      </Provider>
     );
   }
 }
